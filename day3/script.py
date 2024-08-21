8be38a1af9fac51eda065d0082b43b92475248a5
nelson joel amuzu <nelsonjoelamuzu@gmail.com>
2024-08-21 15:36:51 +0000

    Cnsackey

--- day3/script.py
+++ day3/script.py
@@ -1,4 +1,12 @@
 name = input("What is your name")
 age = input("what is your age?")
-y = 'years' if int(age) > 1 else "year"
+y = 'years' if int(age) < 10 else "you are a child"
 print("hello"+" "+ name +" " +"! You are" +" " +age + " " +y+ " " + "old.")
+# message to Cnsacky
+if int(age) < 10:
+    print('hello ' + name + ' you are a child')
+elif 10 < int(age) < 18:
+    print('hello ' + name + ' you are a teenager')
+else:
+    print('hello ' + name + ' you are an adult')
+  
